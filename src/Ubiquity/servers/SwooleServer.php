a802674f35e55cc507fc0af23482ed1d4ba45639
jcheron <myaddressmail@gmail.com>
2019-06-07 14:43:40 +0200

    first commit

---
+++ src/Ubiquity/servers/SwooleServer.php
@@ -0,0 +1,35 @@
+<?php
+namespace Ubiquity\servers;
+
+use Swoole\Http\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+class SwooleServer {
+
+	private $server;
+
+	private $config;
+
+	private $basedir;
+
+	public function init($config, $basedir) {
+		$this->config = $config;
+		$this->basedir = $basedir;
+	}
+
+	public function run($host, $port) {
+		$http = new Server($host, $port);
+		$http->on('start', function ($server) use ($host, $port) {
+			echo "Ubiquity-Swoole http server is started at {$host}:{$port}\n";
+		});
+
+		$http->on("request", function (Request $request, Response $response) {
+			$response->header("Content-Type", "text/plain");
+			$response->end("Hello World\n");
+		});
+
+		$http->start();
+	}
+}
+
