39d49675780080beb687ab76584824d6edcca585
jcheron <myaddressmail@gmail.com>
2019-06-07 17:28:37 +0200

    Add SwooleHttp

--- src/Ubiquity/servers/swoole/SwooleServer.php
+++ src/Ubiquity/servers/swoole/SwooleServer.php
@@ -4,10 +4,17 @@ namespace Ubiquity\servers\swoole;
 use Swoole\Http\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
+use Ubiquity\utils\http\foundation\SwooleHttp;
 
 class SwooleServer {
 
 	private $server;
+
+	/**
+	 *
+	 * @var SwooleHttp
+	 */
+	private $httpInstance;
 
 	private $config;
 
@@ -16,6 +23,7 @@ class SwooleServer {
 	public function init($config, $basedir) {
 		$this->config = $config;
 		$this->basedir = $basedir;
+		$this->httpInstance = new SwooleHttp();
 	}
 
 	public function run($host, $port) {
@@ -25,11 +33,46 @@ class SwooleServer {
 		});
 
 		$http->on("request", function (Request $request, Response $response) {
-			$response->header("Content-Type", "text/plain");
-			$response->end("Hello World\n");
+			$this->handle($request, $response);
 		});
 
 		$http->start();
+	}
+
+	protected function handle(Request $request, Response $response) {
+		$request->get['c'] = '';
+		$this->httpInstance->setResponseCode(200);
+		$uri = ltrim(urldecode(parse_url($request->getUri()->getPath(), PHP_URL_PATH)), '/');
+		if ($uri == null || ! file_exists($this->basedir . '/../' . $uri)) {
+			$request->get['c'] = $uri;
+		} else {
+			$response->end(file_get_contents($this->basedir . '/../' . $uri));
+			return;
+		}
+
+		$this->httpInstance->setRequest($request, $response);
+		$this->parseRequest($request);
+		\ob_start();
+		\Ubiquity\controllers\Startup::setHttpInstance($this->httpInstance);
+		\Ubiquity\controllers\Startup::run($this->config);
+		$content = ob_get_clean();
+		$response->end($content);
+	}
+
+	protected function parseRequest(Request $request) {
+		$headers = [];
+		foreach ($request->header as $key => $value) {
+			if ($key == 'x-forwarded-proto' && $value == 'https') {
+				$request->server['HTTPS'] = 'on';
+			}
+			$headerKey = 'HTTP_' . strtoupper(str_replace('-', '_', $key));
+			$headers[$headerKey] = $value;
+		}
+		$_SERVER = array_change_key_case(array_merge($request->server, $headers), CASE_UPPER);
+		$_GET = $request->get ?? [];
+		$_POST = $request->post ?? [];
+		$_COOKIE = $request->cookie ?? [];
+		$_FILES = $request->files ?? [];
 	}
 }
 
---
+++ src/Ubiquity/utils/http/foundation/SwooleHttp.php
@@ -0,0 +1,100 @@
+<?php
+namespace Ubiquity\utils\http\foundation;
+
+/**
+ * Http instance for Swoole.
+ * Ubiquity\utils\http\foundation$SwooleHttp
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ *         
+ */
+class SwooleHttp extends AbstractHttp {
+
+	private $headers;
+
+	private $responseCode = 200;
+
+	/**
+	 *
+	 * @var \Swoole\Http\Response
+	 */
+	private $response;
+
+	/**
+	 *
+	 * @var \Swoole\Http\Request
+	 */
+	private $request;
+
+	public function getAllHeaders() {
+		return $this->headers;
+	}
+
+	public function header($key, $value, $replace = null, $http_response_code = null) {
+		$this->headers[$key] = $value;
+		if ($http_response_code != null) {
+			$this->responseCode = $http_response_code;
+		}
+		$this->response->header($key, $value);
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getResponseCode() {
+		return $this->responseCode;
+	}
+
+	/**
+	 *
+	 * @param mixed $headers
+	 */
+	private function setHeaders($headers) {
+		foreach ($headers as $k => $header) {
+			if (is_array($header) && sizeof($header) == 1) {
+				$this->headers[$k] = current($header);
+			} else {
+				$this->headers[$k] = $header;
+			}
+		}
+		$this->response->header = $this->response->header + $this->headers;
+	}
+
+	/**
+	 *
+	 * @param int $responseCode
+	 */
+	public function setResponseCode($responseCode) {
+		if ($responseCode != null) {
+			$this->responseCode = $responseCode;
+			$this->response->status($responseCode);
+		}
+	}
+
+	public function headersSent(string &$file = null, int &$line = null) {
+		return headers_sent($file, $line);
+	}
+
+	public function getInput() {
+		return $this->request->getData();
+	}
+
+	/**
+	 *
+	 * @param \Swoole\Http\Request $request
+	 * @param \Swoole\Http\Response $response
+	 */
+	public function setRequest(\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
+		$this->response = $response;
+		$this->request = $request;
+		$default = [
+			'Content-Type' => 'text/html; charset=utf-8',
+			'Server' => $request->request['Server'] ?? 'Swoole'
+		];
+		$this->setHeaders(array_merge($default, $response->getHeaders()));
+	}
+}
+
