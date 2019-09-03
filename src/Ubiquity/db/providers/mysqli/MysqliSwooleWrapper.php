19aefd4e0eb938321a67c48f5810f6673479f592
jcheron <myaddressmail@gmail.com>
2019-09-03 02:03:29 +0200

    Update MysqliSwooleWrapper.php

--- src/Ubiquity/db/providers/mysqli/MysqliSwooleWrapper.php
+++ src/Ubiquity/db/providers/mysqli/MysqliSwooleWrapper.php
@@ -3,7 +3,6 @@ namespace Ubiquity\db\providers\tarantool;
 
 
 use Ubiquity\db\providers\TraitHasPool;
-use Ubiquity\db\provider\mysqli\MysqliWrapper;
 
 class MysqliSwooleWrapper extends MysqliWrapper {
 use TraitHasPool;
