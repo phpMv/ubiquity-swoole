73b0d9d8f46d2e1930efe59ba32116b5250bca6e
jcheron <myaddressmail@gmail.com>
2020-02-19 19:08:28 +0100

    Update TraitHasPool.php

--- src/Ubiquity/db/providers/TraitHasPool.php
+++ src/Ubiquity/db/providers/TraitHasPool.php
@@ -47,9 +47,9 @@ trait TraitHasPool {
 	}
 
 	public function _getStatement(string $sql, $dbInstance = null) {
-		// $key = '_st' . \md5($sql);
-		// return $this->dbInstance->{$key} ??= $this->getStatement($sql, $dbInstance);
-		return $this->getStatement($sql, $dbInstance);
+		$key = '_st' . \md5($sql);
+		$instance = $dbInstance ?? $this->dbInstance;
+		return $instance->{$key} ??= $this->getStatement($sql, $instance);
 	}
 }
 
